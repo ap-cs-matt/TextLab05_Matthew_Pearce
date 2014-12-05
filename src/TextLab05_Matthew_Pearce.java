4ef213d3fea4ad50b5f4f4a653f15752c95fce85
mpearce25 <pearcematthew26@gmail.com>
2014-12-05 07:03:02 -0800

    added animated text fixed int to double errors

--- src/TextLab05_Matthew_Pearce.java
+++ src/TextLab05_Matthew_Pearce.java
@@ -1,104 +1,131 @@
 import java.util.*;
+
 public class TextLab05_Matthew_Pearce {
 
-	 static double num, den;   // numerator and denominator of the rational number
+	static double num, den; // numerator and denominator of the rational number
 
-	   public static void main (String args[])
-	   {
-	      enterData();
+	public static void main(String args[]) {
+		String response;
+		do {
+		enterData();
 
-	      Rational r = new Rational(num,den);
+		Rational r = new Rational(num, den);
 
-	      r.displayData();
-	   }
-	   
-		 public static void enterData()
-	   {
-		  Scanner input = new Scanner(System.in);
-	      System.out.print("\nEnter the numerator ----> ");
-	      num = input.nextInt();
-	      System.out.print("\nEnter the denominator --> ");
-	      den = input.nextInt();
-	   }
+		r.displayData();
+		Animation.animate("\n\nWould you like to repeat this program [Y/N] >>>>");
+		Scanner scan = new Scanner(System.in);
+		response = scan.nextLine(); 
+		}
+		while(response.equalsIgnoreCase("y"));
 	}
 
-	class Rational
+	public static void enterData() {
+		Scanner input = new Scanner(System.in);
+		Animation.animate("\nEnter the numerator ----> ");
+		num = input.nextInt();
+		Animation.animate("\nEnter the denominator --> ");
+		den = input.nextInt();
+	}
+}
+
+class Rational {
+	int num;
+	int den;
+	int gcf;
+
+	public void displayData() {
+
+		
+		Animation.animate("\n" + getOriginal() + " equals " + getDecimal());
+		Animation.animate("\n\nThe reduced form is " + getReduced());
+	}
+
+	public Rational(double eNum, double eDen) {
+		num = (int) eNum;
+		den = (int) eDen;
+	}
+
+	public int getnum() 
 	{
-	   double num;
-	   double den;
-	   int intNum;
-	   int intDen;
-	   int gcf;	  
-	  
-	   public void displayData()
-	   {
-	     
-	      System.out.println("\n" + getOriginal() + " equals " + getDecimal());
-	      System.out.println("\nThe reduced form is " + getReduced());  
-	   }
-	   
-	   public Rational(double eNum , double eDen)
-	   {
-		   num = eNum;
-		   den = eDen;
-		   intNum = (int)eNum;
-		   intDen = (int)eDen;
-	   }
-	  public int getintNum()
-	  {
-		return intNum;  
-	  }
-	  public int getintDen()
-	  {
-		return intDen;  
-	  }
-	  public double getNum()
-	  {
-		return  num;
-	  }
-	  
-	  public double getDen()
-	  {
-		return  den;  
-	  }
-	  
-	  public double getDecimal()
-	  {
-		double decimal = getNum()/getDen();
-		return decimal;   
-	  }
-	  
-	  public String getOriginal()
-	  {
-		String original = getintNum() + "/" + getintDen(); 
-		return original;   
-	  }
-	  
-	 public String getReduced()
-	  {
-		 int gcf = getGCF(intNum, intDen); 
-		 int reducedNum = (intNum/gcf);
-		 int reducedDen = (intDen/gcf);
-		 String reduced =  reducedNum + "/" + reducedDen; 
-		 return reduced;
-	  }
-	  private int getGCF(int n1,int n2)
-	   {
-	      int rem = 0;
-	      do
-	      {
-	         rem = (n1 % n2);
-	         if (rem == 0)
-	            gcf = n2;
-	         else
-	         {
-	            n1 = n2;
-	            n2 = rem;
-	        }
-	      }
-	      while (rem != 0);
-	      
-	      return gcf;
-	   }
-	  
+		return num;
 	}
+
+	public int getden() 
+	{
+		return den;
+	}
+
+	public double getDecimal() {
+		double decimal = (double) getnum() / (double) getden();
+		return decimal;
+	}
+
+	public String getOriginal() {
+		String original = getnum() + "/" + getden();
+		return original;
+	}
+
+	public String getReduced() {
+		int gcf = getGCF(num, den);
+		int reducedNum = (num / gcf);
+		int reducedDen = (den / gcf);
+		String reduced = reducedNum + "/" + reducedDen;
+		return reduced;
+	}
+
+	private int getGCF(int n1, int n2) {
+		int rem = 0;
+		do {
+			rem = (n1 % n2);
+			if (rem == 0)
+				gcf = n2;
+			else {
+				n1 = n2;
+				n2 = rem;
+			}
+		} while (rem != 0);
+
+		return gcf;
+	}
+}
+
+	class Animation
+	{
+		public static void animate(String s)
+		{
+			
+			int length = s.length();
+			int x = 0;
+			String animate = "";
+			
+			while (x <= length-1)
+			{
+				
+					if (x == length-1)
+					{
+						animate =  s.substring(x);
+						System.out.print(animate);
+					}
+					else
+					{
+						animate =  s.substring(x,x+1);
+						System.out.print(animate);
+					}
+						
+							
+				delay(50);
+				x++;
+			}
+		}
+		
+		public static void delay(int time)
+		{
+			long startDelay = System.currentTimeMillis();
+			long endDelay = 0;
+			while (endDelay - startDelay < time)
+				endDelay = System.currentTimeMillis();
+		}
+		
+		
+	}
+
